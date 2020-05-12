d11a60d62f06fbf84af5f970254d4cb1ea06995e
JUDY <s2018s05@e-mirim.hs.kr>
2020-05-12 22:39:00 +0900

    0508 실습일지_07 최대값, 최소값 출력

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace showmeminmaxnumber
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            int i;
+            int[] num= new int[50]; 
+            int max = 0; 
+            int min = 0; 
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("숫자를 입력하세요 : ");
+                num[i] = int.Parse(Console.ReadLine());
+            } 
+
+            //최대값
+            for (i = 0; i < 5; i++)
+            {
+                if (num[i] > max)
+                {
+                    max = num[i];
+              }
+            } 
+
+            //최소값
+            min = num[0];
+            for (i = 0; i < 5; i++)
+            {
+                if (num[i] < min)
+                {
+                    min = num[i];
+              }
+            }
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("가장 큰 수 : " + max);
+            Console.WriteLine("가장 작은 수 : " + min);
         }
     }
-}
+	}
